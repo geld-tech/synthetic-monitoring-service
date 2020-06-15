09ba0e22a413819da367cc562895a10ac06fc3be
zlig <gdevillard@gmail.com>
2020-06-15 14:50:12 +0100

    Add test for other types

--- tests/unit_test/testSyntheticMonitoringApplication.py
+++ tests/unit_test/testSyntheticMonitoringApplication.py
@@ -19,8 +19,16 @@ class TestSyntheticMonitoringApplication(unittest.TestCase):
 
     def test_evaluate_data(self):
         """application.evaluate_data()"""
+        # Test with a Bytes input like Flask's request.data
         data1 = b'{"password":"123456"}'
         evaluated_data = evaluate_data(data1)
+        self.assertIsInstance(evaluated_data, dict)
+        self.assertEqual(evaluated_data, {'password': '123456'})
+        self.assertIsInstance(evaluated_data['password'], str)
+        self.assertEqual(evaluated_data['password'], '123456')
+        # Test with a String input
+        data2 = '{"password":"123456"}'
+        evaluated_data = evaluate_data(data2)
         self.assertIsInstance(evaluated_data, dict)
         self.assertEqual(evaluated_data, {'password': '123456'})
         self.assertIsInstance(evaluated_data['password'], str)

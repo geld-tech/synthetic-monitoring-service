0a91a70a087ea0793f14269987138ec22d23e42c
zlig <gdevillard@gmail.com>
2020-06-15 15:15:35 +0100

    Remove unused code

--- tests/unit_test/testSyntheticMonitoringApplication.py
+++ tests/unit_test/testSyntheticMonitoringApplication.py
@@ -8,26 +8,16 @@ import mock
 
 from application import evaluate_data
 
-# Global config
-local_path = os.path.dirname(os.path.abspath(__file__))
-config_file = local_path+'/../../sources/server'+'/config/settings.cfg'
-secret_file = local_path+'/../../sources/server'+'/config/secret.uti'
-upload_dir = local_path+'/data'
-
 class TestSyntheticMonitoringApplication(unittest.TestCase):
     """TestSyntheticMonitoringApplication Unit Tests"""
 
     def setUp(self):
         """Initialisation"""
-        default_stdout = sys.stdout
-        with open(secret_file, 'w') as f:
-            sys.stdout = f  # Redirect standard output to created file
-            print(os.urandom(24))
-        sys.stdout = default_stdout
+        pass
 
     def tearDown(self):
         """Cleaning-up resources used"""
-        os.remove(secret_file)
+        pass
 
     def test_evaluate_data(self):
         """application.evaluate_data()"""
@@ -45,29 +35,6 @@ class TestSyntheticMonitoringApplication(unittest.TestCase):
         self.assertEqual(evaluated_data, {'password': '123456'})
         self.assertIsInstance(evaluated_data['password'], str)
         self.assertEqual(evaluated_data['password'], '123456')
-
-    def test_get(self):
-        """Data getter"""
-        #nginx_status = NginxStatus()
-        #self.assertEqual(dict, type(nginx_status.get()))
-        # Empty directories evaluate to False in Python
-        #self.assertNotEqual(False, nginx_status.get())
-
-    def test_poll_metrics(self):
-        """Poll Metrics"""
-        #nginx_status = NginxStatus()
-        #self.assertEqual(dict, type(nginx_status.poll_metrics()))
-        # Empty directories evaluate to False in Python
-        #self.assertNotEqual(False, nginx_status.poll_metrics())
-
-    def test_collect_metrics(self):
-        """Collect Metrics"""
-        #nginx_status = NginxStatus()
-        #nginx_status.collect_metrics()
-        #self.assertEqual(dict, type(nginx_status.get()))
-        # Empty directories evaluate to False in Python
-        #self.assertNotEqual(False, nginx_status.get())
-
 
 if __name__ == '__main__':
     unittest.main()

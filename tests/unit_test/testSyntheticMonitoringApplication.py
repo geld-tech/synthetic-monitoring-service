34a58f1d7643ef4ab559c59a0862abf416cafd8c
zlig <gdevillard@gmail.com>
2020-06-15 12:01:47 +0100

    Unit test

---
+++ tests/unit_test/testSyntheticMonitoringApplication.py
@@ -0,0 +1,40 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../sources/server/')
+import unittest
+
+import application
+
+
+class TestSyntheticMonitoringApplication(unittest.TestCase):
+    """NginxStatus Unit Tests"""
+
+    def test_init(self):
+        """Instatiation"""
+        nginx_status = NginxStatus()
+
+    def test_get(self):
+        """Data getter"""
+        nginx_status = NginxStatus()
+        self.assertEqual(dict, type(nginx_status.get()))
+        # Empty directories evaluate to False in Python
+        self.assertNotEqual(False, nginx_status.get())
+
+    def test_poll_metrics(self):
+        """Poll Metrics"""
+        nginx_status = NginxStatus()
+        self.assertEqual(dict, type(nginx_status.poll_metrics()))
+        # Empty directories evaluate to False in Python
+        self.assertNotEqual(False, nginx_status.poll_metrics())
+
+    def test_collect_metrics(self):
+        """Collect Metrics"""
+        nginx_status = NginxStatus()
+        nginx_status.collect_metrics()
+        self.assertEqual(dict, type(nginx_status.get()))
+        # Empty directories evaluate to False in Python
+        self.assertNotEqual(False, nginx_status.get())
+
+
+if __name__ == '__main__':
+    unittest.main()

620d84e9430449820701515cedba82a46ec7d35b
zlig <gdevillard@gmail.com>
2020-06-19 14:50:06 +0100

    Fix path

--- tests/unit_test/testServiceStatus.py
+++ tests/unit_test/testServiceStatus.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../sources/server/')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../../sources/server/')
 import unittest
 
 from modules.ServiceStatus import ServiceStatus
